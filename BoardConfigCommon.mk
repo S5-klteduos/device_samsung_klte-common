98b3e9f29447352a7e8ebaefd6d30c52e17d954e
Sal Savage <salsavage@google.com>
2022-04-15 15:38:17 -0700

    msm8974-common: Replace BTM_DEF_LOCAL_NAME with a sysprop
    Change-Id: I2c90efac01e61500eb1be23bb0ae6f96c2acdc53

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -133,6 +133,7 @@ TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # Qualcomm support
 BOARD_USES_QCOM_HARDWARE := true
