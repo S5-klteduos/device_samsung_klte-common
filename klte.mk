0251d21beeb8cb94a13d4984a7b58384c2877525
Jiakai Zhang <jiakaiz@google.com>
2022-05-20 14:56:44 +0100

    msm8974-common: Preopt SystemUI for AOSP builds.
    Bug: 232860596 | AOSP
    Test: See no dex2oat invocation on the first boot.
    Change-Id: I21b99fac3934060d27fad7323979cbb5f0ed28e8 | AOSP
    (cherry picked from commit 1346647ab4b40afb9d0e737df2ec0cf0f12c2c4d | AOSP)

--- klte.mk
+++ klte.mk
@@ -225,6 +225,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Reduce
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
