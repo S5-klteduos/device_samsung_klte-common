00808a37a0781f2e145eec247c7c4e078a17fa03
1bdellatif <ptaoubi@gmail.com>
2024-04-11 02:44:26 +0000

    msm8974-common: Create dummy libqti-perfd-client

--- common.mk
+++ common.mk
@@ -200,6 +200,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Power HAL
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
